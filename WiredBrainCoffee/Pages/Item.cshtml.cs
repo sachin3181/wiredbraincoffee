965b4120a7f891750025a0ef65439697d7b0a5fb
Sachin Shenoy <sachin3181@gmail.com>
2019-05-29 18:12:00 -0600

    Initial

---
+++ WiredBrainCoffee/Pages/Item.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ItemModel : PageModel
+    {
+        private IMenuService _menuService;
+        public MenuItem Item { get; set; }
+        public ItemModel(IMenuService menuService)
+        {
+           _menuService = menuService;
+        }
+        public void OnGet(int id)
+        {
+           
+            Item = _menuService.GetMenuItems().FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ WiredBrainCoffee/Pages/Menu.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class MenuModel : PageModel
+    {
+        private readonly IMenuService _menuService;
+        public List<MenuItem> Menu { get; set; }
+
+        public MenuModel(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+        public void OnGet()
+        {
+            Menu = _menuService.GetMenuItems();
+           
+        }
+    }
+}
